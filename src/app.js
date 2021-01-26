14bd0afee4ccd367c35e3d336d09ef3c071d39e6
Byeonguk Yoo <bwyoo1229@gmail.com>
2021-01-26 10:34:27 +0900

    Set routes for pages
    - App component should represent what app is doing
      (how app is composed)
    - Use constants to set url for routes for readability and maintainability

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import JumbotronContainer from './containers/jumbotron';
-import FooterContainer from './containers/footer';
-import FaqsContainer from './containers/faqs';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Home } from './pages';
+import { ROUTES } from './constants/routes';
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <BrowserRouter>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </BrowserRouter>
   );
 }
---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+// Sources of urls
+const HOME = '/';
+const BROWSE = '/browse';
+const SIGN_UP = '/signup';
+const SIGN_IN = '/signin';
+
+export const ROUTES = { HOME, BROWSE, SIGN_UP, SIGN_IN };
