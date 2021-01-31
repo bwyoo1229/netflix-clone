5ad7389ed05d890a08af6a72e8eca7e6ef4364b1
Byeonguk Yoo <bwyoo1229@gmail.com>
2021-01-31 21:56:49 +0900

    Save user info in localStorage

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ import { BrowserRouter, Switch } from 'react-router-dom';
 import { Home, Browse, SignIn, SignUp } from './pages';
 import { ROUTES } from './constants/routes';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 export default function App() {
-  const user = '';
+  const user = useAuthListener();
+  console.log(user);
+
   return (
     <BrowserRouter>
       <Switch>
